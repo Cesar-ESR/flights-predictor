bf975835d993c480feed629c0483b9e81791932b
Michael Ziliani <zilianimichael@gmail.com>
2026-01-10 09:23:53 -0500

    2 digit

--- src/main/resources/db/migration/V2__create-table-request.sql
+++ src/main/resources/db/migration/V2__create-table-request.sql
@@ -2,7 +2,7 @@ create table request (
 
     id bigint not null auto_increment,
     flight_datetime timestamp not null,
-    op_unique_carrier char(6) not null,
+    op_unique_carrier char(2) not null,
     origin char(3) not null,
     dest char(3) not null,
     distance double not null,
